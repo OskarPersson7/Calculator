354836d3f57818a78c08808d675baa4599f507ed
OskarPersson7 <oskpera@gmail.com>
2025-12-28 23:59:30 +0100

    reworked keyPadPanel, planned out implementation with comments

--- src/main/java/View/KeypadPanel.java
+++ src/main/java/View/KeypadPanel.java
@@ -2,6 +2,7 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class KeypadPanel extends JPanel {
 
@@ -10,33 +11,23 @@ public class KeypadPanel extends JPanel {
   public KeypadPanel() {
     keyPadPanel.setLayout(new GridLayout(5,4));
 
-    //Detta bör definitvt refaktoriseras
-    keyPadPanel.add(new JButton("AC"));
-    keyPadPanel.add(new JButton("+/-"));
-    keyPadPanel.add(new JButton("%"));
-    keyPadPanel.add(new JButton("÷"));
-    keyPadPanel.add(new JButton("7"));
-    keyPadPanel.add(new JButton("8"));
-    keyPadPanel.add(new JButton("9"));
-    keyPadPanel.add(new JButton("X"));
-    keyPadPanel.add(new JButton("4"));
-    keyPadPanel.add(new JButton("5"));
-    keyPadPanel.add(new JButton("6"));
-    keyPadPanel.add(new JButton("-"));
-    keyPadPanel.add(new JButton("1"));
-    keyPadPanel.add(new JButton("2"));
-    keyPadPanel.add(new JButton("3"));
-    keyPadPanel.add(new JButton("+"));
-    keyPadPanel.add(new JButton("0"));
-    keyPadPanel.add(new JButton("-"));
-    keyPadPanel.add(new JButton("√"));
-    keyPadPanel.add(new JButton("="));
 
-  //gör en lista med Jbuttons som går att iterera
+    //gör en array med Strängrepresentationer som går att iterera
+    String[] buttonValues = {
+        "AC", "+/-", "%", "÷",
+        "7", "8", "9", "×",
+        "4", "5", "6", "-",
+        "1", "2", "3", "+",
+        "0", ".", "√", "="
+    };
 
-  // dela in knappar i 3 grupper
+    // dela in symboler i grupper
+    String[] rightSymbols = {"÷", "×", "-", "+", "="};
+    String[] topSymbols = {"AC", "+/-", "%"};
 
-  //koppla callbacks
+    //iterera över symbolerna och lägg till knappar i panelen
+
+    //koppla callbacks
 
  }
 
